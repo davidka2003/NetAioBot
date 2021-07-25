da6d7a75e87e142759b6e2844b084877226b4e2d
kaletva <k.kaletvincev@gmail.com>
2021-07-26 00:14:18 +0300

    mini redesign

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -2,12 +2,14 @@
 import { ipcRenderer } from 'electron'
 import { remote } from 'electron'
 import React, { ChangeEvent, FormEvent, useEffect,useState } from 'react'
+import '../Login/login.global.scss'
+
 const Login = () => {
     const [key, setkey] = useState(localStorage.getItem("key")||'')
     // let windows = remote.BrowserWindow.getAllWindows()
     // windows.forEach((window,id)=>window.show())
 
-    const login = ()=> 
+    const login = ()=>
     fetch("http://localhost:5000/auth/login", {
         "headers": {
             "accept": "*/*",
@@ -42,13 +44,15 @@ const Login = () => {
     return (
         <div>
             <form onSubmit={loginHandler}>
-                <div className='container'>
-                <h2>NetAio</h2>
+              <div className='login'>
+                <div className='login_content'>
+                  <h1>NetAio</h1>
                   <input className='net_input' placeholder='your key' value={key} onChange={(event:ChangeEvent<HTMLInputElement>)=>setkey(event?.target.value)} id="key" type="text" />
                   <div className='text-center'>
-                    <button className='net_button_primary' type="submit">Login</button>
+                    <button className='net_button_primary login_button' type="submit">Login</button>
                   </div>
                 </div>
+              </div>
             </form>
         </div>
     )
